186ef4b0662b2fdd41d81d5d1ab5911c921ece95
sunlightzhou <sunlight.zhou94@gmail.com>
2024-07-10 19:42:26 +0800

    fix🐛: 更改样式

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,17 +7,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Fractopus",
   keywords: "Fractopus,Connect",
-  description: "Fractopus: A Complex Organism Evolving from the Single Cells of the Internet",
-  openGraph:
-  {
+  description:
+    "Fractopus: A Complex Organism Evolving from the Single Cells of the Internet",
+  openGraph: {
     type: "website",
     url: "https://www.fractopus.net",
     title: "Fractopus",
-    description: "Fractopus: A Complex Organism Evolving from the Single Cells of the Internet",
+    description:
+      "Fractopus: A Complex Organism Evolving from the Single Cells of the Internet",
     siteName: "Fractopus",
-    images: [{
-      url: "https://static.copus.io/images/client/202407/test/65a4f47e4d76c906945b1bf7952cc16b.webp",
-    }],
+    images: [
+      {
+        url: "https://static.copus.io/images/client/202407/test/65a4f47e4d76c906945b1bf7952cc16b.webp",
+      },
+    ],
   },
 };
 
@@ -29,8 +32,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link 
-          href="https://fonts.googleapis.com/css?family=Maven Pro:400,500,600,700,800&display=swap" 
+        <link
+          href="https://fonts.googleapis.com/css?family=Maven Pro:400,500,600,700,800&display=swap"
           rel="stylesheet"
         />
       </head>
